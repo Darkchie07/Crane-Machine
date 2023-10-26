eec0463f3fb75d45a49ee843f78ba02b857b84da
Augusta Steven Benedict <steven02asb@gmail.com>
2023-10-26 20:31:24 +0700

    Fix bug claw movement

--- Assets/ClawHand.cs
+++ Assets/ClawHand.cs
@@ -14,6 +14,8 @@ public class ClawHand : MonoBehaviour
     public stateClaw currentMode;
     public Animator clawAnimation;
     public bool isMoveable;
+    public bool isDoneMove;
+    public bool isBacktoDefault;
 
     [Header("Claw Object")]
     public GameObject rotor;
@@ -26,6 +28,7 @@ public class ClawHand : MonoBehaviour
     public float limitBack;
 
     public float speed;
+    public float delay;
 
     // Start is called before the first frame update
     void Start()
@@ -36,53 +39,96 @@ public class ClawHand : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        switch (currentMode)
+        {
+            case stateClaw.OpenState:
+                isMoveable = true;
+                isDoneMove = false;
+                isBacktoDefault = false;
+                // Implement
+                break;
+            case stateClaw.CloseState:
+                StartCoroutine(GrabbingObject());
+                isMoveable = false;
+                if (isDoneMove)
+                {
+                    currentMode = stateClaw.RisingState;
+                }
+                break;
+            case stateClaw.RisingState:
+                BackToDefault();
+                if (isBacktoDefault)
+                {
+                    currentMode = stateClaw.OpenState;
+                }
+                break;
+        }
+        
         if (isMoveable)
         {
-            if(rotor.transform.position.z < limitBack)
+            if(Input.GetKey(KeyCode.UpArrow))
             {
                 //move back
-                if (Input.GetKey(KeyCode.UpArrow))
+                if (rotor.transform.position.z < limitBack)
                 {
                     rotor.transform.Translate(0, 0, speed * Time.deltaTime);
+                    clawHands.transform.Translate(0, 0, speed * Time.deltaTime);
                 }
             }
-            else if(rotor.transform.position.z > limitFront)
+            else if(Input.GetKey(KeyCode.DownArrow))
             {
                 //move front
-                if (Input.GetKey(KeyCode.DownArrow))
+                if (rotor.transform.position.z > limitFront)
                 {
                     rotor.transform.Translate(0, 0, speed * -1 * Time.deltaTime);
+                    clawHands.transform.Translate(0, 0, speed * -1 * Time.deltaTime);
                 }
             }
-            else if(clawHands.transform.position.x > limitLeft)
+            else if(Input.GetKey(KeyCode.LeftArrow))
             {
                 //move Left
-                if (Input.GetKey(KeyCode.LeftArrow))
+                if (clawHands.transform.position.x > limitLeft)
                 {
                     clawHands.transform.Translate(speed * -1 * Time.deltaTime, 0, 0);
                 }
             }
-            else if(clawHands.transform.position.x < limitRight)
+            else if(Input.GetKey(KeyCode.RightArrow))
             {
                 //move Right
-                if (Input.GetKey(KeyCode.RightArrow))
+                if (clawHands.transform.position.x < limitRight)
                 {
                     clawHands.transform.Translate(speed * Time.deltaTime, 0, 0);
                 }
             }
-        }
-
-        switch (currentMode)
-        {
-            case stateClaw.OpenState:
+            else if (Input.GetKey(KeyCode.Space))
+            {
                 clawAnimation.SetTrigger("Open");
-                break;
-            case stateClaw.CloseState:
-                clawAnimation.SetTrigger("Close");
-                break;
-            case stateClaw.RisingState:
-                // Implement jumping logic
-                break;
+                currentMode = stateClaw.CloseState;
+            }
+        }
+    }
+
+    public IEnumerator GrabbingObject()
+    {
+        yield return new WaitForSeconds(delay);
+        clawAnimation.SetTrigger("Close");
+        yield return new WaitForSeconds(delay);
+        isDoneMove = true;
+    }
+
+    public void BackToDefault()
+    {
+        if (rotor.transform.position.z >= limitFront)
+        {
+            rotor.transform.Translate(0, 0, limitFront * Time.deltaTime);
+            clawHands.transform.Translate(0, 0, limitFront * Time.deltaTime);
+        }
+        else if (clawHands.transform.position.x >= limitLeft)
+        {
+            clawHands.transform.Translate(limitLeft * Time.deltaTime, 0, 0);
+        }else if (rotor.transform.position.z <= limitFront && clawHands.transform.position.x <= limitLeft)
+        {
+            isBacktoDefault= true; 
         }
     }
 }
